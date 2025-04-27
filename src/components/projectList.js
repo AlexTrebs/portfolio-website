6e666a5b6af561c5549dcacea0d3b4d52d5ccb7d
Alex <snalex2539@gmail.com>
2025-04-27 22:16:13 +0100

    feature/markdown-implementation * Implemented Markdown loading from link for project walkthroughs. * Fixed minor bugs

--- src/components/projectList.js
+++ src/components/projectList.js
@@ -1,32 +1,29 @@
 import * as pug from 'pug';
+import * as fs from 'node:fs';
 
 // Sample projects data (simulate your NoSQL DB documents)
 const projects = [
   {
     _id: "tictactoe",
     title: "TicTacToe^2",
-    image: "/assets/app1.jpg",
+    icon: "/assets/app1.jpg",
     remote: "http://localhost:3001",  // Not used in this example
     enable: true,
-    description: "A simple game from childhood I developed so that I didn\'t have to carry around paper anymore!",
-    demo: "/demos/app1.mp4",
-    gif: "/assets/app1_demo.gif"
+    markdown: fs.readFileSync('./shortcut.md').toString()
   },
   {
     _id: "project2",
     title: "Simple App",
-    image: "/assets/app2.jpg",
+    icon: "/assets/app2.jpg",
     remote: "/apps/app2.html",
     enable: true,
-    description: "A simple app for daily tasks."
   },
   {
     _id: "project3",
     title: "Demo App",
-    image: "/assets/app3.jpg",
+    icon: "/assets/app3.jpg",
     remote: "/apps/app3.html",
     enable: true,
-    demo: "/demos/app3.mp4"
   }
 ];
 
